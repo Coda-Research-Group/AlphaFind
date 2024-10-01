94b93853b4b8c6139aa6f77bdd793c90a1a7de40
Terézia Slanináková <445526@mail.muni.cz>
2024-10-01 09:16:38 +0200

    add tests, rename scipts to "_", add to ci

--- training/run.sh
+++ training/run.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 # 1) ---- Create embeddings
-python3 create-embedding.py --input=./data/cifs --output=./data/embedding.pkl --granularity 10
+python3 create_embedding.py --input=./data/cifs --output=./data/embedding.pkl --granularity 10
 
 # 2) ---- Create a K-Means object
 # Clusters and saves the k-means object to to `data/kmeans.idx`
@@ -13,7 +13,7 @@ python3 train.py --input=./data/embedding.pkl --kmeans-path=data/kmeans.idx --ou
 
 # 4) ---- Create bucket-data
 # Collects all predictions from the newest model in `models/`, and saves them to `bucket-data/`
-python3 create-buckets.py --input=./data/embedding.pkl --model-dir-path=./models/ --output-chunks=./data/chunks --output-predictions=./data/overall --output-bucket-path ./data/bucket-data/
+python3 create_buckets.py --input=./data/embedding.pkl --model-dir-path=./models/ --output-chunks=./data/chunks --output-predictions=./data/overall --output-bucket-path ./data/bucket-data/
 
 # 5) ---- Create bucket-data mapping to protein IDs
-python3 create-protein-bucket-mapping.py --bucket-path=./data/bucket-data/ --output=./data/bucket-mapping.pkl
+python3 create_protein_bucket_mapping.py --bucket-path=./data/bucket-data/ --output=./data/bucket-mapping.pkl
