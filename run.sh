56c3828658b2f61f9f7f4c0518e4d9310cc9e382
Jakub Čillík <524749@mail.muni.cz>
2024-11-01 13:43:07 +0100

    Unify docker image naming scheme

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@
 (
     cd training || exit 1
 
-    docker build -t alphafind:train -f ./Dockerfile .
+    docker build -t alphafind/training:run -f ./Dockerfile .
 
     docker run \
         -v /$(pwd)/data:/training/data \
         -v /$(pwd)/models:/training/models \
         -e WANDB_MODE=offline \
-        alphafind:train
+        alphafind/training:run
 )
 
 # API
@@ -21,22 +21,22 @@
     ln -s ../training/models models
     ln -s ../training/data data
 
-    docker build -t alphafind:server -f ./server/Dockerfile .
+    docker build -t alphafind/api:run -f ./server/Dockerfile .
 
     # Run the server in the background
     docker run -d -p 8080:8000 \
         -v /$(pwd)/data:/data \
         -v /$(pwd)/models:/models \
         -v /$(pwd)/eph:/eph \
-        alphafind:server
+        alphafind/api:run
 )
 
 # UI
 (
     cd ui || exit 3
 
-    docker build -t alphafind:ui -f ./Dockerfile .
+    docker build -t alphafind/ui:run -f ./Dockerfile .
 
     # Run the UI in the background
-    docker run -d -p 8081:8081 alphafind:ui
+    docker run -d -p 8081:8081 alphafind/ui:run
 )
