a820de906a5b0c0517fc9028d7931de824ff7175
Jakub Čillík <524749@mail.muni.cz>
2024-11-01 13:33:14 +0100

    Added pytests for kmeans and models

---
+++ training/tests/test_create_kmeans.py
@@ -0,0 +1,30 @@
+import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+from alphafind_training.create_embedding import create_embedding
+from alphafind_training.cluster import create_kmeans
+
+
+def _setup(tempdir: str):
+    create_embedding(
+        input_path=Path("./data/cifs"),
+        output_path=Path(f"{tempdir}/embedding.pkl"),
+        granularity=10
+    )
+
+
+def test_create_kmeans():
+    with TemporaryDirectory() as tempdir:
+        _setup(tempdir)
+        
+        output_path = f"{tempdir}/kmeans.idx"
+
+        create_kmeans(
+            input_path=f"{tempdir}/embedding.pkl",
+            output_path=output_path,
+            n_clusters=2
+        )
+
+        assert os.path.exists(output_path)
